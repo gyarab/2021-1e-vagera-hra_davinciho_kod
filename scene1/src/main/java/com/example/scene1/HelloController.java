0c387d8ace6880b817e2b417e5080787e376233b
JakubVagera <94869479+JakubVagera@users.noreply.github.com>
2022-05-03 08:18:22 +0200

    Add files via upload

---
+++ scene1/src/main/java/com/example/scene1/HelloController.java
@@ -0,0 +1,53 @@
+package com.example.scene1;
+import javafx.beans.property.BooleanProperty;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import javafx.scene.control.Button;
+
+
+public class HelloController {
+    @FXML
+    Button oj;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+   public void switchtoScene1(ActionEvent event) throws IOException {
+       Parent root = FXMLLoader.load(getClass().getResource("vyber_pomlcka_hrac.fxml"));
+       stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+       scene = new Scene(root);
+       stage.setScene(scene);
+       stage.show();
+       System.out.println("Hello world!");
+   }
+
+
+    public void switchtoScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchtoScene3(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("vyber_pomlcky.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void end(ActionEvent event) throws IOException{
+       System.exit(0);
+    }
+}
---
+++ scene1/src/main/java/com/example/scene1/ending_buttons.java
@@ -0,0 +1,11 @@
+package com.example.scene1;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class ending_buttons {
+    public void end(ActionEvent event) throws IOException {
+        System.exit(0);
+    }
+}
